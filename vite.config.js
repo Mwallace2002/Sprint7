f4f221f71a13e9c9e1771e50701df66e9a48b17d
Mwallace2002 <Maxwallace2002@gmail.com>
2024-06-09 02:07:00 -0400

    Arreglo viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA(),
-  ],
-  server: {
-    middlewareMode: 'ssr',
-    // Middleware para establecer el tipo MIME correcto para los archivos JSX
-    fs: {
-      strict: false,
-    },
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
-});
+  plugins: [react()],
+})
