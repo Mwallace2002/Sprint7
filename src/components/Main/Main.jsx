96ba749bd5754c72c605a85215908ae7ec6e6619
Mwallace2002 <Maxwallace2002@gmail.com>
2024-06-09 18:04:26 -0400

    first commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,27 +5,37 @@ import Home from '../Home/Home.jsx';
 import Delivery from '../Delivery/Delivery.jsx'; 
 import Vehiculos from '../Vehiculos/Vehiculos.jsx';
 import Visitas from '../Visitas/Visitas.jsx';
-
-import { isTokenValid } from '../../utils/auth.js';
+import PrivateRoute from '../PrivateRoute/PrivateRoute.jsx';
 
 const Main = () => {
-    const token = localStorage.getItem('token');
-    const tokenExistAndStillValid = isTokenValid(token);
-
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/delivery" element={<Delivery />} /> 
-                <Route path="/vehiculos" element={<Vehiculos />} /> 
-                <Route path="/visitas" element={<Visitas />} /> 
-                <Route path="*" element={<Navigate to="/" />} /> 
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        } />
+        <Route path="/delivery" element={
+          <PrivateRoute>
+            <Delivery />
+          </PrivateRoute>
+        } />
+        <Route path="/vehiculos" element={
+          <PrivateRoute>
+            <Vehiculos />
+          </PrivateRoute>
+        } />
+        <Route path="/visitas" element={
+          <PrivateRoute>
+            <Visitas />
+          </PrivateRoute>
+        } />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default Main;
-
-
